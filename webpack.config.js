39b72118c78d1be0c956b6b0fa82cab9f30492cc
JANS66 <98973687+JANS66@users.noreply.github.com>
2026-03-23 11:39:14 +0200

    Initial setup with Webpack, HTML, and CSS plugins

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { watchFile } = require('fs');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    watchFiles: ['./src/template.html'],
+  },
+};
